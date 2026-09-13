d170038e9201c4be359f58cd30062f41f4b82dcd
watch-r <nabil202051@gmail.com>
2026-09-13 08:44:06 +0600

    Add initial game setup with player mechanics and level design
    - Created player scene with animations and jump sound effects.
    - Implemented player movement and jumping logic in player.gd script.
    - Added level_1 scene with tilemap and background.
    - Integrated main scene to load level_1.
    - Included music and icon assets for the game.
    - Configured project settings for display and input handling.

---
+++ Scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+const SPEED = 300
+const JUMP_VELOCITY = -850
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+
+func _physics_process(_delta: float) -> void:
+	handle_gravity(_delta)
+	add_animation()
+	handle_jump()
+	handle_movement()
+	move_and_slide()
+
+func handle_gravity(_delta) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * _delta
+
+func handle_jump() -> void:
+	if Input.is_action_just_pressed('jump') and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+	if not is_on_floor():
+		animated_sprite_2d.animation = "jumping"
+
+func handle_movement() -> void:
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	# flip direction
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	elif direction == -1.0:
+		animated_sprite_2d.flip_h = true
+
+func add_animation() -> void:
+	if velocity.x > 1 or velocity.x < -1:
+		animated_sprite_2d.animation = "running"
+	else:
+		animated_sprite_2d.animation = "idle"
